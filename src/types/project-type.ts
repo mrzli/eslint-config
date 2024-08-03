7d52ec92e8534ad403de58f793f8b381abf2b299
Goran Mržljak <goran.mrzljak@gmail.com>
2024-08-03 16:05:51 +0200

    lint fix

--- src/types/project-type.ts
+++ src/types/project-type.ts
@@ -1,3 +1,8 @@
-const PROJECT_TYPE_LIST = ['react', 'browser', 'node', 'shared'] as const;
+export const PROJECT_TYPE_LIST = [
+  'react',
+  'browser',
+  'node',
+  'shared',
+] as const;
 
 export type ProjectType = (typeof PROJECT_TYPE_LIST)[number];
